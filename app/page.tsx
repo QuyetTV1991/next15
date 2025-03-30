85c027fc436c946ac81737afffd5359c003cbb0a
QuyetTV1991 <quyettv.bk@gmail.com>
2025-03-30 18:35:34 +0800

    global css and next Theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-auth-dark">
-      <h1 className="text-primary-500 px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+      <h1 className="text-primary-500 px-4 sm:px-8 py-2 sm:py-3 bg-light850-dark100 flex-center">
         Welcome from Next JS
       </h1>
       <button className="p-4 text-dark-300 fill-white border-2 rounded-md shadow-light-100">
